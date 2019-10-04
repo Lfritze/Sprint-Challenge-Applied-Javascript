8cef5485f9a6dc1e1aff82416b60034e6d2332f6
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-04 11:25:14 -0400

    Completed Step 1, header component and function

--- components/Header/index.js
+++ components/Header/index.js
@@ -12,3 +12,34 @@
 //initial commit for Emily
 // test
 function Header() {}
+
+const headerContainer = document.querySelector('.header-container')
+    headerContainer.appendChild(Header());
+
+// Here I am going in a specific order for waterfall purposes...
+//Define, class, content, structure
+function Header () {
+    // DEFINE --- .createElement()
+    const header = document.createElement('div'); // parent
+        const date = document.createElement('span'); //kid
+        const title = document.createElement('h1'); // kid
+        const temp = document.createElement('span'); // kid
+     
+    //Add Class classList.add()
+    // header, date, temp
+    header.classList.add('header'); // parent
+        date.classList.add('date'); // kid
+        temp.classList.add('temp'); // kid
+
+    //Set content .textContent =
+    title.textContent = 'Lambda Times';
+    date.textContent = 'Smarch 28, 2019';
+    temp.textContent = '98°';
+
+    // structure .appendChild()  // append children to const header
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    return header;
+}
